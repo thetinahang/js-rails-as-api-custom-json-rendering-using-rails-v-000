b26ee35abc8b097b08dbb76b9eadca75c4916328
Tina Hang <mstinahang@gmail.com>
2020-01-13 01:06:20 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,6 +6,6 @@ class BirdsController < ApplicationController
 
   def show
     bird = Bird.find_by(id: params[:id])
-      render json: birds, only: [:id, :name, :species]
+    render json: birds, only: [:id, :name, :species]
   end
 end
